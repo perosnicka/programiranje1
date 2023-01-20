0567e1812b77a2f1189808287a6f4194bdb577c7
Simon Klun <simonklun8@gmail.com>
2023-01-20 09:50:43 +0100

    vaja2

---
+++ Vaja2/Vaja2/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Vaja2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Stari Donald farmo ima");
+            Console.WriteLine("IA IA O");
+            Console.WriteLine("Na tej farmi osla ima");
+            Console.WriteLine("IA IA O");
+        }
+    }
+}
