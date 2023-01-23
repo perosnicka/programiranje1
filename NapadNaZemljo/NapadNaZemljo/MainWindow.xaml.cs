ae4995154da0b78521a42de032d7044377bd09a2
Peter <gantarpeter11@gmail.com>
2023-01-23 10:59:59 +0100

    NapadNaZemljo

--- NapadNaZemljo/NapadNaZemljo/MainWindow.xaml.cs
+++ NapadNaZemljo/NapadNaZemljo/MainWindow.xaml.cs
@@ -9,6 +9,7 @@ using System.Windows.Data;
 using System.Windows.Documents;
 using System.Windows.Input;
 using System.Windows.Media;
+using System.Windows.Media.Animation;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
@@ -20,9 +21,45 @@ namespace NapadNaZemljo
     /// </summary>
     public partial class MainWindow : Window
     {
+        Random r = new Random();
+
         public MainWindow()
         {
             InitializeComponent();
+        }
+
+        private void Start_Click(object sender, RoutedEventArgs e)
+        {
+            DodajSovraznika();
+        }
+
+        private void DodajSovraznika()
+        {
+            ContentControl sovraznik = new ContentControl();
+            sovraznik.Template = Resources["PredlogaSovražnik"] as ControlTemplate;
+            AnimirajSovraznika(sovraznik, 0, (int) (IgralniProstor.ActualWidth - 100), "(Canvas.Left)");
+            int visina = (int) (IgralniProstor.ActualHeight - 100);
+            AnimirajSovraznika(sovraznik, r.Next(visina), r.Next(visina), "(Canvas.Top)");
+        }
+
+        private void AnimirajSovraznika(ContentControl sovraznik, int v1, double v2, string v3)
+        {
+            Storyboard zgodba = new Storyboard()
+            {
+                AutoReverse = true,
+            RepeatBehavior = RepeatBehavior.Forever
+            };
+            DoubleAnimation animacija = new DoubleAnimation()
+            {
+                From = v1,
+                To = v2,
+                Duration = new Duration (TimeSpan.FromSeconds(r.Next(4, 6)))
+            };
+            PropertyPath x = new PropertyPath(v3);
+            Storyboard.SetTarget(animacija, sovraznik);
+            Storyboard.SetTargetProperty(animacija, x);
+            zgodba.Children.Add(animacija);
+            zgodba.Begin();
         }
     }
 }
