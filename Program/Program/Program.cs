98f5611eb702b1bb81d9227f6f3b22b24b72d2ca
Peter <gantarpeter11@gmail.com>
2023-01-27 10:59:53 +0100

    vaja izpis

---
+++ Program/Program/Program.cs
@@ -0,0 +1,21 @@
+﻿public class Program {
+    public static void Main(string[] args) {
+        double [] meseci = new double[12];
+        double vsota = 0;
+        for (int i = 0; i <= meseci.Length; i++) {
+            Console.WriteLine("Vnesi povprečno temperaturo za "+(i+1)+". mesec: ");
+            meseci[i] = double.Parse(Console.ReadLine());
+            vsota = vsota + vsota;
+        }
+
+        double povp = vsota / 12;
+        Console.WriteLine("Povprečna temperatura: "+povp);
+        Console.WriteLine("Mrzli meseci:");
+        for(int i = 0; i < meseci.Length; i++){
+            if (meseci[i] < povp)
+                Console.WriteLine((i + 1) + ".mesec");
+        }
+
+
+    }
+}
---
+++ VajaIzpis/VajaIzpis/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace VajaIzpis
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            
+            int a = 20;
+            double b = -13.6667;
+            int c = 17;
+            double d = 12.88;
+            double e = 2222.879;
+            Console.WriteLine("{0, 20:f2}", a);
+            Console.WriteLine("{0, 23:f4}", b);
+            Console.WriteLine("{0, 20:f2}", c);
+            Console.WriteLine("{0, 20:f2}", d);
+            Console.WriteLine("{0, 22:f2}", e);
+            Console.ReadLine();
+            
+
+            
+
+        }
+    }
+}
