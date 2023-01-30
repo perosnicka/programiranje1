17204ab76bf5923d94436dcc81ef273a1e49d176
Peter <gantarpeter11@gmail.com>
2023-01-30 10:59:22 +0100

    karte

---
+++ karta/karta/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace karta
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Random k = new Random();
+            int x = k.Next(4);
+            karta karta = (karta)x;
+            Console.WriteLine("karta: "+karta);
+
+            
+
+
+            Random v = new Random();
+            int y= v.Next(13);
+            Vrednost vrednost = (Vrednost)y;
+            Console.WriteLine("vrednost: " + vrednost);
+
+            Console.ReadLine();
+        }
+    }
+}
---
+++ karta/karta/karta.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace karta
+{
+    enum karta
+    {
+        srce=1, karo, pik, križ
+    }
+    enum Vrednost
+    {
+        ace=1, dvojka, trojka, štirica, petka, šestka, sedemka, osemka, devetka, desetka, fant, dama, kralj
+    }
+}
