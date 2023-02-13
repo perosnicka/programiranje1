59fa8ddf21e4fc245af2ff24f9e5709451234b17
Peter <gantarpeter11@gmail.com>
2023-02-13 10:58:27 +0100

    zanke2

---
+++ zanke2/zanke2/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace zanke2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int st = 0;
+            for (int k = (int)'A'; k <= (int)'Z'; k++)
+            {
+                Console.Write((char)k);
+                st++;
+                if(st == 10)
+                {
+                    Console.WriteLine("\n");
+                    st = 0;
+                }
+
+            }
+            
+        }
+    }
+}
