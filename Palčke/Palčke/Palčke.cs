38c1dafa003d55f9f05387b3d66dd63d49ae8ad8
Peter <gantarpeter11@gmail.com>
2023-02-20 10:55:54 +0100

    palčke

--- Palčke/Palčke/Palčke.cs
+++ Palčke/Palčke/Palčke.cs
@@ -8,5 +8,56 @@ namespace Palčke
 {
     internal class Palčke
     {
+        int StPalck;//število palčk na mizi
+        int Igralec;//1 ali 2
+
+        public int Igralec1 { get => Igralec;}
+        public int StPalck1 { get => StPalck;}
+        public Palčke()
+        {
+            Igralec = 1;
+            StPalck = 11;
+        }
+        public Palčke(int st)
+        {
+            StPalck = st;
+            Igralec = 1;
+        }
+        public Palčke(int st, int i)
+        {
+            StPalck = st;
+            Igralec = i;
+        }
+        public void izpisStanja()
+        {
+            Console.WriteLine("Na mizi je: "+StPalck+" palčk.");
+            Console.WriteLine("Na vrsti je: "+Igralec);
+        }
+        //vzemi n palčk iz mize in zamenjaj igralca
+        public string izpisStanja1()
+        {
+            string r = "";
+            r = r+"Na mizi je: " + StPalck + " palčk.";
+            r = r+"\nNa vrsti je: " + Igralec;
+            return r;
+        }
+        public void VzemiPalcke(int n)
+        {
+            if (n < 1 || n > 3)
+            {
+                Console.WriteLine("Ne slepari!");
+                return;
+            }
+            StPalck = StPalck - n;
+            Igralec = 3 - Igralec;
+        }
+        public bool JeKonec()
+        {
+            return StPalck <= 0;
+        }
+        public int DobiZmagovalca()
+        {
+            return Igralec;
+        }
     }
 }
--- Palčke/Palčke/Program.cs
+++ Palčke/Palčke/Program.cs
@@ -10,6 +10,16 @@ namespace Palčke
     {
         static void Main(string[] args)
         {
+            Palčke p = new Palčke(23, 2);
+            while (!p.JeKonec())
+            {
+                Console.WriteLine(p.izpisStanja1());
+                Console.WriteLine("Koliko palčk vzameš? ");
+                int x = int.Parse(Console.ReadLine());
+                p.VzemiPalcke(x);
+            }
+            Console.WriteLine("Zmagal je: "+p.DobiZmagovalca());
+            Console.ReadLine();
         }
     }
 }
