3d31ff35a4fdaaa2ea93196183c91614f0936d46
Peter <gantarpeter11@gmail.com>
2023-02-24 09:46:59 +0100

    pravokotnik2

---
+++ Liki/Liki/Program.cs
@@ -0,0 +1,23 @@
+﻿
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Liki
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            pravokotnik p = new pravokotnik(0, 1, 1, 0);
+            Console.WriteLine("Koordinati kota levo zgoraj = " + p.X1 + "," + p.Y1 +
+            "\nKoordinati kota desno spodaj = " + p.X2 + "," + p.Y2);
+            Console.WriteLine("Stranica a " + p.StranicaA());
+            Console.WriteLine("Stranica b " + p.StranicaB());
+            Console.WriteLine("Plošina " + p.Ploscina());
+            Console.ReadLine();
+        }
+    }
+}
---
+++ Liki/Liki/pravokotnik.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Liki
+{
+    internal class pravokotnik
+    {
+        int x1;
+        int y1;
+        int x2;
+        int y2;
+        public pravokotnik(int x1p, int y1p, int x2p, int y2p )
+        {
+            x1 = x1p;
+            y1 = y1p;
+            x2 = x2p;
+            y2 = y2p;
+        }
+
+        public int X1 { get => x1; set => x1 = value; }
+        public int Y1 { get => y1; set => y1 = value; }
+        public int X2 { get => x2; set => x2 = value; }
+        public int Y2 { get => y2; set => y2 = value; }
+
+        public int StranicaA()
+        {
+            return Math.Abs(x2 - x1);
+        }
+        public int StranicaB()
+        {
+            return Math.Abs(y2 - y1);
+        }
+        public int Ploscina()
+        {
+            return StranicaA() * StranicaB();
+        }
+    }
+}
