f52f2b2bb2af67b83836a6e102f6ae64afa2ee3c
Peter <gantarpeter11@gmail.com>
2023-03-01 12:44:36 +0100

    tabele

--- TabeleVaja/TabeleVaja/Program.cs
+++ TabeleVaja/TabeleVaja/Program.cs
@@ -19,13 +19,23 @@ namespace TabeleVaja
                 vsota += ocene[k];
             }
             Console.WriteLine("Povprečje je: " + ((double)vsota / ocene.Length));
-            Console.ReadLine();*/
+            Console.ReadLine();
 
             int[] a = { 80, 3, 7, 9, 1, 5, 6, 84, 15 };
             Console.WriteLine("Neurejena tabela");
             Izpis(a);
-            Izbiranje(a);
-
+            Vstavljanje(a);*/
+            int[] a = new int[500000];
+            Random r = new Random();
+            for (int k = 0; k < a.Length; k++)
+            {
+                a[k] = r.Next(100000);
+            }
+            DateTime dt1 = DateTime.Now;
+            Vstavljanje(a);
+            DateTime dt2 = DateTime.Now;
+            Double ts = (dt2 - dt1).TotalMilliseconds;
+            Console.WriteLine("za izbiranje "+ts+" ms");
             Console.ReadLine();
         }
         static void Izpis(int[] t)
@@ -56,8 +66,24 @@ namespace TabeleVaja
                 int c = t[k];
                 t[k] = t[minIndeks];
                 t[minIndeks] = c;
-                Izpis(t);
+                //Izpis(t);
             }//konec for
+        }
+        static void Vstavljanje(int[] t)
+        {
+            for (int k = 1; k < t.Length; k++)
+            {
+                //Pogrezni element t[k]
+                int j = k;
+                int temp = t[k];
+                while (j > 0 && t[j - 1] > temp)
+                {
+                    t[j] = t[j - 1];
+                    j--;
+                }//ali smo dobili manjšega od temp ali je konec tabele
+                t[j] = temp;
+                //Izpis(t);
+            }
         }
     }
 }
